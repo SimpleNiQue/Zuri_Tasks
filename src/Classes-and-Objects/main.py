50f220b06e87cdbe91c05475933b76ddbbb0497c
simplenick <simplenick01@gmail.com>
2022-05-28 06:48:36 +0100

    Fixed classes and objects

--- src/Classes-and-Objects/main.py
+++ src/Classes-and-Objects/main.py
@@ -1,37 +1,44 @@
 class Student:
     """This Is a student class"""
 
+
     def __init__(self, name:str, age: int, score: float, tracks:list ) -> None:
         self.__name = name
         self.__age = age
         self.__score = score
         self.__tracks = tracks
 
+
     def __str__(self):
         return self.__name
 
+    
     def change_name(self, new_name: str) -> str:
         self.__name = new_name
         return 'Name Changed!!'
 
+
     def change_age(self, new_age: str)-> str:
+
+        assert new_age.isdigit, f'new age ({new_age}) cannot be {type(new_age)}'
+        
         self.__age = new_age
         return 'Age Changed!!'
+
 
     def add_track(self, new_track: list) -> str:
         self.__tracks += new_track
         return 'New track added!!'
 
+
     def get_score(self) -> str:
         return self.__score
 
 
-
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+if __name__ == '__main__':
+    Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
 # Expected methods
 n = Bob.change_name("Peter")
-a = Bob.change_age(34)
+a = Bob.change_age('hello')
 t = Bob.add_track("UI/UX")
-print(Bob.get_score())
-print(Bob)
